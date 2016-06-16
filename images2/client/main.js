a3aeee8d952918417171c7b1afa85f783e38a3ae
Paul Savignano <Paul.Savignano@gmail.com>
2016-06-16 14:42:11 -0700

    Added components and used state to render response form ajax call

--- images2/client/main.js
+++ images2/client/main.js
@@ -1,22 +1,26 @@
-import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import axios from 'axios'
+import ImageList from './components/image_list'
 
-import './main.html';
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { images: [] }
+  }
+  componentWillMount() {
+    axios.get('https://api.imgur.com/3/gallery/hot/viral/0')
+      .then(response => this.setState({ images: response.data.data }))
+  }
+  render() {
+    return (
+      <div>
+        <ImageList images={this.state.images} />
+      </div>
+    )
+  }
+}
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
-});
-
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
-
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
-});
+Meteor.startup(() => {
+  ReactDOM.render(<App />, document.querySelector('.container'))
+})
